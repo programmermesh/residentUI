59428f52f15489b448fb548a955b2349f9f8147f
Raphael Olajide <tayoraph@gmail.com>
2023-02-09 16:35:03 +0100

    sidebar added

---
+++ src/app/layout/home/home.module.ts
@@ -0,0 +1,37 @@
+// import { NgModule } from '@angular/core';
+// import { HttpClientModule } from '@angular/common/http';import { RouterModule } from '@angular/router';
+// import { CommonModule } from '@angular/common';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
+
+// import { DashboardComponent } from '../dashboard/dashboard.component';
+// import { HomeRoutes } from './home.router';
+// import { RegisterComponent } from 'src/app/auth/register/register.component';
+// import { TopnavComponent } from '../topnav/topnav.component';
+// import { SidebarComponent } from '../sidebar/sidebar.component';
+// import { HomeComponent } from './home.component';
+// import { FooterComponent } from '../footer/footer.component';
+// import { HeaderComponent } from '../header/header.component';
+
+// @NgModule({
+//   imports: [
+//     CommonModule,
+//     RouterModule.forChild(HomeRoutes),
+//     FormsModule,
+//     ReactiveFormsModule,
+//     HttpClientModule
+//   ],
+//   declarations: [
+//     DashboardComponent,
+//     RegisterComponent,
+//     HeaderComponent,
+//     FooterComponent,
+//     HomeComponent,
+//     SidebarComponent,
+//     TopnavComponent,
+//   ]
+// })
+
+// export class HomeModule {}
---
+++ src/app/layout/home/home.router.ts
@@ -0,0 +1,14 @@
+// import { Routes } from '@angular/router';
+// import { LoginComponent } from 'src/app/auth/login/login.component';
+// import { RegisterComponent } from 'src/app/auth/register/register.component';
+// import { DashboardComponent } from '../dashboard/dashboard.component';
+// import { HomeComponent } from './home.component';
+
+// ;
+
+// export const HomeRoutes: Routes = [
+//     { path: 'dashboard',      component: DashboardComponent },
+//     {path: 'login', component: LoginComponent},
+//     {path: 'register', component: RegisterComponent},
+//     {path: 'home', component: HomeComponent},
+// ];
